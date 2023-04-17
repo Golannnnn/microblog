6db9c3d2f5a9a3b4e6974517ebbf3f63153b4067
Golannnnn <forburacc@protonmail.ch>
2023-04-17 18:49:38 +0300

    implemented live updates and infinite scrolling

--- src/components/TweetsList.jsx
+++ src/components/TweetsList.jsx
@@ -1,10 +1,30 @@
+import { useEffect } from "react";
 import { useAuth } from "../lib/AuthContext";
 import { useTweet } from "../lib/TweetContext";
 import Tweet from "./Tweet";
 
 const TweetsList = () => {
   const { currentUser } = useAuth();
-  const { tweets, loadingTweets } = useTweet();
+  const { tweets, loadingTweets, handleBatches, lastKey } = useTweet();
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastKey]);
+
+  const handleScroll = () => {
+    const offsetHeight = document.documentElement.offsetHeight,
+      innerHeight = window.innerHeight,
+      scrollTop =
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop ||
+        0;
+    if (offsetHeight - innerHeight - scrollTop < 1 && lastKey) {
+      handleBatches(lastKey);
+    }
+  };
+
   const results =
     currentUser &&
     tweets.map((tweet) => {
@@ -18,7 +38,25 @@ const TweetsList = () => {
       );
     });
 
-  return <>{!loadingTweets && results}</>;
+  return (
+    <>
+      {!loadingTweets && (
+        <>
+          {results}
+          {/* {lastKey && (
+            <button
+              onClick={() => handleBatches(lastKey)}
+              style={{
+                backgroundColor: "white",
+              }}
+            >
+              Load more
+            </button>
+          )} */}
+        </>
+      )}
+    </>
+  );
 };
 
 export default TweetsList;
--- src/lib/TweetContext.jsx
+++ src/lib/TweetContext.jsx
@@ -5,6 +5,8 @@ import {
   orderBy,
   query,
   onSnapshot,
+  limit,
+  startAfter,
 } from "firebase/firestore";
 import { createContext, useContext, useEffect, useState } from "react";
 import { useAuth } from "./AuthContext";
@@ -18,7 +20,9 @@ export const useTweet = () => {
 
 export const TweetProvider = ({ children }) => {
   const [tweets, setTweets] = useState([]);
+  const [lastKey, setLastKey] = useState(null);
   const [loadingTweets, setLoadingTweets] = useState(false);
+  const [initialLoad, setInitialLoad] = useState(true);
   const { currentUser } = useAuth();
 
   const getAllTweets = async () => {
@@ -33,6 +37,7 @@ export const TweetProvider = ({ children }) => {
 
   const checkForDuplicates = (prev, change) => {
     const duplicates = prev.filter((tweet) => tweet.id === change.doc.id);
+
     if (duplicates.length > 0) {
       return prev;
     }
@@ -41,17 +46,94 @@ export const TweetProvider = ({ children }) => {
       date: change.doc.data().date.toDate(),
       id: change.doc.id,
     };
-    return [newTweet, ...prev];
+
+    return [newTweet, ...prev].sort((a, b) => b.date - a.date);
+  };
+
+  // const getFirstBatch = async () => {
+  //   if (!tweets || tweets.length === 0) return;
+  //   try {
+  //     const q = query(
+  //       collection(db, "tweets"),
+  //       orderBy("date", "desc"),
+  //       limit(10)
+  //     );
+  //     const querySnapshot = await getDocs(q);
+
+  //     let tweets = [];
+  //     let lastKey;
+  //     querySnapshot.forEach((doc) => {
+  //       tweets.push({
+  //         ...doc.data(),
+  //         date: doc.data().date.toDate(),
+  //         id: doc.id,
+  //       });
+  //       lastKey = doc.data().date;
+  //     });
+  //     return { tweets, lastKey };
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
+  const getNextBatch = async (key) => {
+    try {
+      const q = query(
+        collection(db, "tweets"),
+        orderBy("date", "desc"),
+        startAfter(key),
+        limit(10)
+      );
+      const querySnapshot = await getDocs(q);
+      let tweets = [];
+      let lastKey;
+      querySnapshot.forEach((doc) => {
+        // check if tweet already exists
+        if (tweets.some((tweet) => tweet.id === doc.id)) return;
+        tweets.push({
+          ...doc.data(),
+          date: doc.data().date.toDate(),
+          id: doc.id,
+        });
+        lastKey = doc.data().date;
+      });
+      return { tweets, lastKey };
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleBatches = (key) => {
+    if (key && currentUser && !loadingTweets) {
+      getNextBatch(key)
+        .then((data) => {
+          // check if data.tweets contains tweets already in state
+          // if so, filter them out
+          const filteredTweets = data.tweets.filter(
+            (tweet) => !tweets.some((t) => t.id === tweet.id)
+          );
+          setLastKey(data.lastKey);
+          setTweets((prev) => [...prev, ...filteredTweets]);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
   };
 
   useEffect(() => {
+    if (!currentUser || loadingTweets) return;
     setLoadingTweets(true);
-    const q = query(collection(db, "tweets"), orderBy("date", "asc"));
-    if (!currentUser) return;
+    const q = query(
+      collection(db, "tweets"),
+      orderBy("date", "desc"),
+      limit(10)
+    );
     const unsubscribe = onSnapshot(q, (snapshot) => {
       snapshot.docChanges().forEach((change) => {
         if (change.type === "added") {
           setTweets((prev) => checkForDuplicates(prev, change));
+          setLastKey(change.doc.data().date);
         }
       });
       setLoadingTweets(false);
@@ -64,6 +146,8 @@ export const TweetProvider = ({ children }) => {
     loadingTweets,
     currentUser,
     postTweet,
+    handleBatches,
+    lastKey,
   };
 
   return (
